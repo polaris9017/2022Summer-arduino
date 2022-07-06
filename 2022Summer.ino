51d5f3a493057beae9c83ae0e429c710215eda18
polaris9017 <moonrise917@gmail.com>
2022-07-06 18:34:57 +0900

    Implement functions
    화재 감지 모듈, 블루투스 수신 모듈 중간 스케치

--- 2022Summer.ino
+++ 2022Summer.ino
@@ -1,23 +1,5 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
-/*
-    Name:       2022Summer.ino
-    Created:	2022-07-02 오후 5:32:37
-    Author:     DESKTOP-FEJUQIH\Kim.K
-*/
+/* Fire Detecting System implementation source */
 
-// Define User Types below here or use a .h file
-//
-
-
-// Define Function Prototypes that use User Types below here or use a .h file
-//
-
-
-// Define Functions below here or use other .ino or cpp files
-//
-
-// The setup() function runs once each time the micro-controller starts
 #include <OneWire.h>
 #include <DallasTemperature.h>  // Temperature sensor library
 #include <Wire.h>
@@ -33,12 +15,24 @@
 #define LED_ON 255
 #define LED_OFF 0
 
+#define LCD_CHARS 16
+
+// 측정하고 실제값 입력할 것
+#define TEMP_LIMIT 0
+#define LIGHT_LIMIT 0
+
 LiquidCrystal_I2C lcd(0x3f, 16, 2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature tempSensor(&oneWire);
 
-SoftwareSerial BSerial(5, 6); //BLE module | Tx to 5, Rx to 6
+// Clear line on display
+void clearLine(int line) {
+    for (int i = 0; i < LCD_CHARS; i++) {
+        lcd.setCursor(i, line);
+        lcd.print(" ");
+    }
+}
 
 /**
 * Print string to display
@@ -46,7 +40,8 @@ SoftwareSerial BSerial(5, 6); //BLE module | Tx to 5, Rx to 6
 * @param line : line in display (range: 0 ~ 1)
 * @param pos : position in display (range: 0~15)
 */
-void display(String msg, int line, int pos) {  // print to display
+void display(const String& msg, int line, int pos) {  // print to display
+    clearLine(line);
     lcd.setCursor(pos, line);
     lcd.print(msg);
 }
@@ -62,16 +57,6 @@ int get_light_level() {
     return analogRead(LIGHT_SENSOR_BUS);
 }
 
-// set BLE module to scan mode
-void scan_ble() {
-    delay(1000);
-    BSerial.println("AT+SCAN");  //Scan mode
-}
-
-void led_beep() {
-    analogWrite(LED_PIN_RED, 255);
-}
-
 //LED module test
 void led_init_test() {
     Serial.println("Testing LED module");
@@ -79,8 +64,8 @@ void led_init_test() {
     Serial.println("255 -> 0");
     for (int val = 255; val > 0; val--) {
         analogWrite(LED_PIN_RED, val);
-        analogWrite(LED_PIN_BLUE, 255-val);
-        analogWrite(LED_PIN_GREEN, 128-val);
+        analogWrite(LED_PIN_BLUE, 255 - val);
+        analogWrite(LED_PIN_GREEN, 128 - val);
         delay(1);
     }
 
@@ -95,8 +80,29 @@ void led_init_test() {
     Serial.println("LED test done!");
 }
 
-void toggle_led(uint8_t color, int mode) {
-    analogWrite(color, mode);
+void toggle_led(uint8_t color) {
+    if (analogRead(color) == LED_OFF)
+        analogWrite(color, LED_ON);
+    else
+        analogWrite(color, LED_OFF);
+}
+
+void led_beep() {
+    if (analogRead(LED_PIN_RED) == LED_ON) analogWrite(LED_PIN_RED, LED_OFF);
+    toggle_led(LED_PIN_RED);
+    delay(250);
+    toggle_led(LED_PIN_RED);
+    delay(250);
+}
+
+bool detect_temp_warning() {
+    if (get_temperature() > TEMP_LIMIT) return true;
+    return false;
+}
+
+bool detect_light_warning() {
+    if (get_light_level() < LIGHT_LIMIT) return true;
+    return false;
 }
 
 void setup() {
@@ -111,13 +117,17 @@ void setup() {
     led_init_test();
 
     Serial.begin(9600);  //baud rate
-    //Serial.println("d");
 }
 
 // Add the main program code into the continuous loop() function
 void loop() {
-    display("test message!", 0, 1);
-    display("Temp: " + String(get_temperature()), 0, 1);
-    display("Light: " + String(get_light_level()), 0, 1);
-    toggle_led(LED_PIN_RED, LED_ON);
+    display("Temp: " + String(get_temperature()), 0, 0);
+    display("Light: " + String(get_light_level()), 1, 0);
+
+    if (detect_light_warning() && detect_temp_warning()) {
+        clearLine(0);
+        clearLine(1);
+        display("Fire!", 0, 0);
+        led_beep();
+    }
 }
---
+++ BeaconReciever.ino
@@ -0,0 +1,57 @@
+/* Beacon Reciever implementation source */
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>  // LCD Display library
+
+#define LCD_CHARS 16
+
+SoftwareSerial BSerial(5, 6); //BLE module | Tx to 5, Rx to 6
+LiquidCrystal_I2C lcd(0x3f, 16, 2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+// Clear line on display
+void clearLine(int line) {
+    for (int i = 0; i < LCD_CHARS; i++) {
+        lcd.setCursor(i, line);
+        lcd.print(" ");
+    }
+}
+
+/**
+* Print string to display
+* @param msg : String to display
+* @param line : line in display (range: 0 ~ 1)
+* @param pos : position in display (range: 0~15)
+*/
+void display(String msg, int line, int pos) {  // print to display
+    clearLine(line);
+    lcd.setCursor(pos, line);
+    lcd.print(msg);
+}
+
+// set BLE module to scan mode
+void scan_ble() {
+    Serial.println("Setting BLE module to scan mode");
+    delay(1000);
+    BSerial.println("AT+SCAN");  //Scan mode
+}
+
+void setup() {
+    // write your initialization code here
+    Serial.begin(9600);
+    Serial.println("Initializing...");
+    BSerial.begin(9600);
+
+    scan_ble();
+}
+
+void loop() {
+    // write your code here
+    if (BSerial.available()) {
+        String readVal = BSerial.readString();
+        Serial.println("Recieved : " + readVal);
+        delay(100);
+
+        //일단 아두이노에 블루투스 모듈 물려서 출력값 확인하고 구현할 것
+    }
+}
